080defeb192c1806d5e039c15a9ea64c9dd59418
GUO YANKE <hi@yankeguo.com>
2024-01-10 17:28:43 +0800

    feat: add /sentence_segmentation

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY ./requirements.txt /code/requirements.txt
 
 RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
 
+ENV STANZA_RESOURCES_DIR /data/stanza_resources
+
 ADD . .
 
 CMD ["gunicorn", "-k", "sync", "-w", "1", "-b", "0.0.0.0:7860", "main:app"]
--- main.py
+++ main.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import stanza
+
+nlp = stanza.Pipeline("en", processors="tokenize")
 
 app = Flask(__name__)
 
+
 @app.route("/")
-def hello():
+def action_index():
     return "Hello, World!"
+
+
+@app.route("/sentence_segmentation", methods=["POST"])
+def action_sentences_segment():
+    doc = nlp(request.get_json()["document"])
+    return jsonify({"sentences": [sentence.text for sentence in doc.sentences]})
