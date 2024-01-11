a8d486a529de1ce0eab37936a8f7ecd8703c5893
GUO YANKE <hi@yankeguo.com>
2024-01-11 09:52:43 +0800

    feat: add secret_key validation

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, abort
 import os
 import stanza
+
+secret_key = os.getenv("SECRET_KEY", "")
 
 nlp = stanza.Pipeline("en", processors="tokenize", download_method=None)
 
 app = Flask(__name__)
+
+
+def guard_secret_key():
+    if request.args.get("secret_key", "") == secret_key:
+        return
+    if request.headers.get("X-Secret-Key", "") == secret_key:
+        return
+    abort(401)
 
 
 @app.route("/")
@@ -14,5 +24,6 @@ def action_index():
 
 @app.route("/sentence_segmentation", methods=["POST"])
 def action_sentences_segment():
+    guard_secret_key()
     doc = nlp(request.get_json()["document"])
     return jsonify({"sentences": [sentence.text for sentence in doc.sentences]})
